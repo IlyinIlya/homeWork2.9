02a05bfe0fe693b6af2a3398b808d9db2d7d0b69
Ilyin Ilya <ilyin88ok@gmail.com>
2024-05-31 20:56:38 +0330

    1. Start

---
+++ src/main/java/com/ArtCompany/ArtCompanyProduction/ArtCompanyProductionApplication.java
@@ -0,0 +1,13 @@
+package com.ArtCompany.ArtCompanyProduction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtCompanyProductionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtCompanyProductionApplication.class, args);
+	}
+
+}
